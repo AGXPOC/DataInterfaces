010f272acd97d6be7a628822b58cd2339eb25f86
akhileshbangera <41292652+akhileshbangera@users.noreply.github.com>
2020-06-30 13:07:26 +0530

    Updated s3_select.py
    Added dynamic query changes.

--- lambda/s3_select.py
+++ lambda/s3_select.py
@@ -2,24 +2,59 @@ import boto3
 import os
 import base64
 import json
+import sys
+from botocore.session import Session
+from botocore.config import Config
 
-def lambda_handler(event, context):
+def lambda_handler(s3_obj_dtls_dict, context):
     
-    print('event:',event)
-    l_bucket_name = event['bucket']
-    l_file_name = event['key']
-    l_sql_query =   event['sql_query']
-    l_return_obj = {1:1}
+    
+    l_bucket_name = s3_obj_dtls_dict['bucket_name']
+    l_file_name = s3_obj_dtls_dict['file_name']
+    
+#---------------------------------------------------------------------------------------------------------------------------    
+    projection_list = ''
+    i = 0
+    for key in s3_obj_dtls_dict['projection']:
+        if i == 0:
+            projection_list = ' s.'+ key +' '
+        else:
+            projection_list = projection_list+', s.' + key +' '
+         
+        i=i+1
+     
+#---------------------------------------------------------------------------------------------------------------------------  
+    filter_query = ''
+    
+    i = 0
+    for rec in s3_obj_dtls_dict['selection']:
+        if rec:
+                
+            if i ==0:
+                filter_query = 'WHERE s.'+ rec['col_name'] + ' ' +  rec['operator'] + " '"  + rec['value']+ "'"
+            else:
+                filter_query = filter_query + ' AND s.'+ rec['col_name'] + ' ' +  rec['operator'] + " '"  + rec['value']+ "'"
+                
+            i = i+1
+
+#---------------------------------------------------------------------------------------------------------------------------        
+    s3_select_query = 'SELECT ' + projection_list +' FROM s3object s ' +  filter_query
+    
     
     s3 = boto3.client('s3')
     
-   
+    print('S3 query: ',s3_select_query)
+    print('Bucket_name: ',l_bucket_name)
+    print('File_name: ',l_file_name)
     
     response = s3.select_object_content(Bucket = l_bucket_name,
                                         Key = l_file_name,
                                         ExpressionType = 'SQL',
-                                        Expression = l_sql_query,
-                                        InputSerialization = {'CSV': {#"FileHeaderInfo": "NONE"
+                                        Expression = s3_select_query,
+                                        InputSerialization = {'CSV': {#"FileHeaderInfo": "USE",
+                                                                      "AllowQuotedRecordDelimiter" : True,
+                                                                       'RecordDelimiter': '\n',
+                                                                        'FieldDelimiter': '\t'
                                         }},
                                         OutputSerialization = {'CSV': {
                                             'FieldDelimiter': ',',
@@ -28,23 +63,27 @@ def lambda_handler(event, context):
     
     #payload_cpy = response['Payload']
     
-    for event in response['Payload']:
-        print("in for loop")
-        if 'Records' in event:
-            
-            records = event['Records']['Payload'].decode('utf-8')
-            #records = event['Records']['Payload']
-            
-        elif 'Stats' in event:
-            statsDetails = event['Stats']['Details']
-            print("Bytes scanned: ")
-            print(statsDetails['BytesScanned'])
-            print("Bytes processed: ")
-            print(statsDetails['BytesProcessed'])
-            
+    try:
+        records = ''
+        for event in response['Payload']:
+            print('---------------------------------')
+            if 'Records' in event:
+              
+
+                records = event['Records']['Payload'].decode('utf-8')
+                print('Payload fetch complte')
+                
+            elif 'Stats' in event:
+                statsDetails = event['Stats']['Details']
+                print("Bytes scanned: ")
+                print(statsDetails['BytesScanned'])
+                print("Bytes processed: ")
+                print(statsDetails['BytesProcessed'])
         
-    #json.dumps(base64.b64encode(bytes(records, encoding='utf8'))),   
-    #base64.b64encode(bytes(records, encoding='utf8')),
+    except:
+        e = sys.exc_info()
+        print('exception occured:',e)
+        
         
     return_file=  {   "statusCode": 200,
               "headers": {
@@ -57,15 +96,4 @@ def lambda_handler(event, context):
                                     
                     
     
-    return return_file;              
-
-           
-		   
-'''
-{
-  "bucket": "sec.data.05.16.2020.1",
-  "key": "NYSE.csv",
-  "sql_query": "Select s._1,s._2,s._3 from s3object s where s._2 = 'ASP'"
-}
-
-'''		   
+    return return_file;  
